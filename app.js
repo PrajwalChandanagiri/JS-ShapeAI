d6bebcdede06b38f65ec965649c9252d71724ed8
PrajwalChandanagiri <prajwalchandanagiri1911@gmail.com>
2021-09-28 18:56:23 +0530

    JS Console Strings Numbers and Bollen

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log("heloo World")
 console.warn("hello Warning")
-console.error("hello Error")
+console.error(typeof"hello Error")
+
+//Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof -1)
+console.log(typeof 12365466646546)
+console.log(typeof -1.1)
+
+//boolen
+console.log(typeof true)
+console.log(typeof false)
+
